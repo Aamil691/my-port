cb76cdf7ea5bdf442d34b66a58b99620fd31bf02
Aamil691 <aamilrais34@gmail.com>
2025-07-06 15:37:11 +0530

    Delete Login.js

--- Login.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const LoginSchema = new mongoose.Schema({
-  email: { type: String, required: true },
-  timestamp: { type: Date, default: Date.now }
-});
-
-module.exports = mongoose.model('Login', LoginSchema); 
